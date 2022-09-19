2e01521079f47e77dc1602e8e45ef6628604fc39
Aljay Pascual <aljay.pascual@norimaconsulting.com>
2022-09-19 17:03:16 +0800

    first commits

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        titleDeviderAnimation: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(130px)" },
-        },
-        entranceAnimation: {
-          "0%": { opacity: "0", transform: "translateY(-50px)" },
-          "100%": { opacity: "1", transform: "	transform: translateY(0)" },
-        },
-      },
-      animation: {
-        "left-to-right": "titleDeviderAnimation 3s ease-in-out infinite",
-        "entrance": "entranceAnimation  2s ease 0s 1 normal forwards",
-      },
+  //     keyframes: {
+  //       titleDeviderAnimation: {
+  //         "0%": { transform: "translateX(0)" },
+  //         "100%": { transform: "translateX(130px)" },
+  //       },
+  //       entranceAnimation: {
+  //         "0%": { opacity: "0", transform: "translateY(-50px)" },
+  //         "100%": { opacity: "1", transform: "	transform: translateY(0)" },
+  //       },
+  //     },
+  //     animation: {
+  //       "left-to-right": "titleDeviderAnimation 3s ease-in-out infinite",
+  //       "entrance": "entranceAnimation  2s ease 0s 1 normal forwards",
+  //     },
     },
   },
   plugins: [],
